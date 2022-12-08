0e936e8c7db3bbd997f445c62bd3bee99b34cf68
Noe De Santo <noe.desanto@epfl.ch>
2022-12-08 23:17:34 +0100

    Solve day 1

---
+++ README.md
@@ -0,0 +1,1 @@
+More info [here](https://adventofcode.com/2022/about).
---
+++ src/Day01.scala
@@ -0,0 +1,20 @@
+//> using lib "com.lihaoyi::os-lib:0.9.0"
+
+def parseInput: List[Int] = {
+  val in = os.read(os.pwd / "inputs" / "day_01.txt")
+  in.split("\n\n").map(bag => bag.split("\n").map(Integer.parseInt).foldLeft(0)(_ + _)).toList
+}
+
+def part1 = {
+  println(s"Part 1: ${parseInput.max}")
+}
+
+def part2 = {
+  println(s"Part 2: ${parseInput.sorted(summon[Ordering[Int]].reverse).take(3).reduce(_ + _)}")
+}
+
+@main
+def main = {
+  part1
+  part2
+}
